1d30fd0041b8be70b8fff68837fe742bce21ca42
Andrej <andrej@vitech.dev>
2025-03-26 21:21:05 +0100

    ServicesPage added and styles improved

--- src/pages/ExperiencePage.vue
+++ src/pages/ExperiencePage.vue
@@ -8,7 +8,7 @@
       <div class="experience-item">
         <h3 class="role-title">Software Engineer at Vitech (Feb 2023 - present):</h3>
         <p class="role-description">
-          Building many full stack applications for different purposes, with JavaScript, Node.js, GraphQL, PostgreSQL on
+          🔹 Building many full stack applications for different purposes, with JavaScript, Node.js, GraphQL, PostgreSQL on
           the backend, and JavaScript and Vue.js on the frontend.
           <br>Also, implementing AI features in our software products.
         </p>
@@ -17,7 +17,7 @@
       <div class="experience-item">
         <h3 class="role-title">BSS Operations Specialist at A1 Macedonia (Jun 2022 - Dec 2022):</h3>
         <p class="role-description">
-          Responsible for ensuring availability and performance of the databases that support the system.
+          🔹 Responsible for ensuring availability and performance of the databases that support the system.
           <br>Responsible for troubleshooting and problem solving of SQL queries with proactively monitoring the database
           systems.
         </p>
@@ -26,7 +26,7 @@
       <div class="experience-item">
         <h3 class="role-title">Data Analyst Intern at A1 Macedonia (Feb 2022 - Jun 2022):</h3>
         <p class="role-description">
-          Analyzing and organizing data and marketing results. Creating and optimizing prediction models with Machine
+          🔹 Analyzing and organizing data and marketing results. Creating and optimizing prediction models with Machine
           Learning Algorithms,
           <br>where I worked on two prediction models: one for Improving Sales and one for Fraud Prediction Model.
         </p>
--- src/pages/ServicesPage.vue
+++ src/pages/ServicesPage.vue
@@ -1,5 +1,31 @@
 <template>
   <main class="services-page">
+    <div class="header-container">
+      <h1 class="page-title">Services</h1>
+      <h3 class="page-subtitle">What I offer 💻</h3>
+    </div>
+
+    <div class="services-content">
+      <h1 class="content-title">Frontend Developer < ></h1>
+      <ul class="content-container">
+        <li>🔹 I create clean, responsive, and visually appealing interfaces.</li>
+        <li>🔹 I optimize websites for speed and smooth performance.</li>
+        <li>🔹 I implement AI-driven functionalities for smarter applications.</li>
+        <li>🔹 I ensure websites work seamlessly on all devices.</li>
+      </ul>
+    </div>
+
+    <div class="services-content">
+      <h1 class="content-title">Backend Developer 🗄</h1>
+      <ul class="content-container">
+        <li>🔹 I design and build secure, scalable, high-performance APIs.</li>
+        <li>🔹 I develop efficient backend logic for seamless functionality.</li>
+        <li>🔹 I integrate AI features and handle large-scale data processing.</li>
+        <li>🔹 I optimize and maintain databases for high performance.</li>
+        <li>🔹 I implement secure authentication, login and data protection.</li>
+        <li>🔹 I deploy and manage applications on cloud platforms like AWS.</li>
+      </ul>
+    </div>
 
   </main>
 </template>
