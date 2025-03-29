817523100a3f7776c5b4d8eb34a388e44f5692b8
Andrej <andrej@vitech.dev>
2025-03-29 16:38:19 +0100

    updates

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -45,17 +45,17 @@
       <a href="mailto:andrej.atanasovski.00@gmail.com">
         <img class="contact-icon" src="../assets/icons/email-icon.svg" alt="email">
       </a>
-      <a href="https://www.facebook.com/andrej.atanasovski.14/" target="_blank">
-        <img class="contact-icon" src="../assets/icons/facebook-icon.svg" alt="facebook">
-      </a>
-      <a href="https://www.instagram.com/aatanasovskii/" target="_blank">
-        <img class="contact-icon" src="../assets/icons/instagram-icon.svg" alt="instagram">
-      </a>
       <a href="https://www.linkedin.com/in/andrej-atanasovski-a2000/" target="_blank">
         <img class="contact-icon" src="../assets/icons/linkedin-icon.svg" alt="linkedin">
       </a>
       <a href="https://github.com/aatanasovskii" target="_blank">
         <img class="contact-icon" src="../assets/icons/github-icon.svg" alt="github">
+      </a>
+      <a href="https://www.facebook.com/andrej.atanasovski.14/" target="_blank">
+        <img class="contact-icon" src="../assets/icons/facebook-icon.svg" alt="facebook">
+      </a>
+      <a href="https://www.instagram.com/aatanasovskii/" target="_blank">
+        <img class="contact-icon" src="../assets/icons/instagram-icon.svg" alt="instagram">
       </a>
     </section>
   </div>
