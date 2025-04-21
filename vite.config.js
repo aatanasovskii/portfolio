25b7d1037d431189587fdc0a7aa3df224d07698c
Andrej <andrej@vitech.dev>
2025-04-21 21:56:57 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: import.meta.env.VITE_BASE_PATH || '/portfolio/',
+  base: process.env.VITE_BASE_PATH || '/portfolio/',
   plugins: [
     vue(),
     vueDevTools(),
