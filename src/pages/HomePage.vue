0b5014ead280dc421ef1095d8678207fb8269d0c
Andrej <andrej@vitech.dev>
2025-04-21 23:35:53 +0200

    cv updated

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -35,7 +35,7 @@ export default {
   name: 'HomePage',
   methods: {
     openCV() {
-      window.open('/portfolio/src/assets/CV-Andrej.pdf', '_blank');
+      window.open('/portfolio/CV-Andrej.pdf', '_blank');
     }
   }
 }
